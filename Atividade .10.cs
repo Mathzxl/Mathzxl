98599a912fd9b0370e68954596d9f848661a9995
Mathzxl <162761845+Mathzxl@users.noreply.github.com>
2024-03-10 18:35:13 -0300

    Update and rename Atividade 10.cs to Atividade .10.cs

---
+++ Atividade .10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       /*10. Faça um programa que receba a altura e o sexo de uma pessoa e calcule e mostre seu peso
+ideal, utilizando as seguintes fórmulas (onde h corresponde à altura):
+• Homens: (72,7 * h) - 58
+• Mulheres: (62, 1 * h) – 44, 7*/
+            double A, sx, pi;
+            Console.WriteLine("Dê a altura de uma pessoa");
+            A = double.Parse(Console.ReadLine());
+            Console.WriteLine("De o sexo de uma pessoa(H/M)");
+            sx = char.Parse(Console.ReadLine());
+            if (sx == 'H' || sx == 'h')
+            {
+                pi = (72.7 * A) - 58;
+                Console.WriteLine("O peso ideal do Homem sera de: " + pi);
+            }
+            else
+            {
+                pi = (62.1 * A) - 44.7;
+                Console.WriteLine("O peso ideal da Mulher séria de: " + pi);
+            }
+            Console.ReadLine();
+        }
+    }
+}
